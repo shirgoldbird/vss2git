8b049a62827823d1f60575f872e7d1f49f3e0996
Shir Goldberg <sgoldberg@adaptavist.com>
2021-05-07 14:17:47 -0700

    add missing import

--- Vss2Git/GitExporter.cs
+++ Vss2Git/GitExporter.cs
@@ -18,6 +18,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
